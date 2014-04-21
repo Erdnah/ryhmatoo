1398a35100df98653656730b1767be99434ba2ee
Erdnah <handreelias@gmail.com>
2014-04-21 23:28:41 +0300

    asd

--- src/ee/oop/mang/Main.java
+++ src/ee/oop/mang/Main.java
@@ -9,29 +9,53 @@ import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	Random rand = new Random();
+	static double SCENE_X;
+	static double SCENE_Y;
 	
 	@Override
 	public void start(final Stage lava) {
 		Group juur = new Group();
 		final Scene scene = new Scene(juur, 1000, 500);
 		
-		final int radius = 25;
+		final int radius = 30;
 		
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 40; i++) {
 			final Circle circle = new Circle(radius);
 			Physics.setRandomXY(circle);
-			circle.setFill(Color.rgb(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
-			circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+			if (i < 10)
+				circle.setFill(Color.RED);
+			else if (i > 10 && i < 20)
+				circle.setFill(Color.GREEN);
+			else if (i > 20 && i< 30)
+				circle.setFill(Color.BLUE);
+			else
+				circle.setFill(Color.YELLOW);
+			circle.setStroke(Color.BLACK);
+			circle.setOnMouseDragged(new EventHandler<MouseEvent>() {
+
 				@Override
 				public void handle(MouseEvent event) {
-					Physics.move(circle, event.getX(), event.getY()*-1);
+					SCENE_X = scene.getWidth();
+					SCENE_Y = scene.getHeight();
+					//System.out.println(scene.getHeight());
+					Physics.drag(circle, event.getSceneX(), event.getSceneY());
 				}
 			});
+			/*circle.setOnMouseEntered(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					if (event.isPrimaryButtonDown()) {
+						System.out.println("omg");
+						Physics.move(circle, event.getX(), event.getY()*-1);
+					}
+				}
+			});*/
 			juur.getChildren().add(circle);
 		}
 		lava.setScene(scene);
