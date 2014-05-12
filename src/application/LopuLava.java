a3f5cea38093aca62d337caea32d248e3c0f58fd
Erdnah <handreelias@gmail.com>
2014-05-12 21:27:47 +0300

    struktruur

---
+++ src/application/LopuLava.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class LopuLava extends Stage {
+	
+	public void init() {
+		BorderPane juur = new BorderPane();
+		final Scene lõpp = new Scene(juur, 320, 90);
+		Button lopp = new Button("Lõpeta");
+		Button uuesti = new Button("Uuesti");
+		HBox hb3 = new HBox();
+		juur.setCenter(ManguLava.tekst);
+		juur.setBottom(hb3);
+		hb3.setSpacing(10);
+		hb3.setPadding(new Insets(10));
+		hb3.getChildren().addAll(uuesti, lopp);
+		lopp.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				Platform.exit();
+			}
+		});
+		setTitle("Lõpp");
+		setScene(lõpp);
+		show();
+	}
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,18 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -31,47 +22,16 @@ public class Main extends Application {
 	static int SCENE_Y = 600;
 	static Color color = Color.YELLOW;
 	static int palle = 4; // MITU PALLI TAHAD?
-	final int radius = 30;
+	final static int radius = 30;
 	int ulejoone = 0;
-	long startTime = 0;
+	static long startTime = 0;
+	static TextField arv = null;
 
-	ArrayList<Circle> seesPallid = new ArrayList<Circle>();
-	
-	// Tere tulemast
-	// See meetod kontrollib,kas antud ring asub väljaspool kaste
-	public boolean kasVäljas(Circle circle) {
-		if (circle.getLayoutX() < 125 && circle.getLayoutY() < 125
-				&& circle.getFill() == Color.GREEN) {
-
-			return false;
-		} else if (circle.getLayoutX() > SCENE_X - 125
-				&& circle.getLayoutY() < 125 && circle.getFill() == Color.RED) {
-
-			return false;
-		} else if (circle.getLayoutX() > SCENE_X - 125
-				&& circle.getLayoutY() > SCENE_Y - 125
-				&& circle.getFill() == Color.BLUE) {
-
-			return false;
-		} else if (circle.getLayoutX() < 125
-				&& circle.getLayoutY() > SCENE_Y - 125
-				&& circle.getFill() == Color.YELLOW) {
-
-			return false;
-		} else {
-			return true;
-		}
-	}
 
 	public void start(final Stage alguslava) {
-		Group juur = new Group();
-		BorderPane juur2 = new BorderPane();
-		BorderPane juur3 = new BorderPane();
-		final Scene algus = new Scene(juur2, 260, 150);
-		final Scene lõpp = new Scene(juur3, 370, 90);
-		final Label tekst = new Label();
-		final Stage mangulava = new Stage();
-		mangulava.setResizable(false);
+
+		BorderPane juur = new BorderPane();
+		final Scene algus = new Scene(juur, 260, 150);
 
 		Label ylesanne = new Label(
 				"Ülesanne on "
@@ -80,142 +40,39 @@ public class Main extends Application {
 
 		HBox hb1 = new HBox();
 		HBox hb2 = new HBox();
-		VBox vb1 = new VBox();
+		
 		hb1.setPadding(new Insets(10));
 		hb2.setPadding(new Insets(10));
-		vb1.setSpacing(10);
-		vb1.setPadding(new Insets(10));
-		hb1.getChildren().add(ylesanne);
-		vb1.getChildren().add(tekst);
+		
 
-		juur3.setCenter(vb1);
+		
 		Button start = new Button("Alusta");
-		Button lopp = new Button("Lõpeta");
-		vb1.getChildren().add(lopp);
-		lopp.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				Platform.exit();
-			}
-		});
-		final TextField arv = new TextField("Sisesta enda nimi");
-		hb2.getChildren().add(start);
-		juur2.setTop(hb1);
-		juur2.setBottom(hb2);
-		juur2.setCenter(arv);
-		final Scene scene = new Scene(juur, SCENE_X, SCENE_Y);
-		mangulava.setScene(scene);
 		// Anname start nupule tegevuse
 		start.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				mangulava.show();
+				ManguLava mangulava = new ManguLava();
+				mangulava.init();
 				startTime = System.currentTimeMillis();
 				alguslava.close();
 			}
 
 		});
-		// Loome vastavad kastid nurkadessse
-		Rectangle vasak1 = new Rectangle(15, 15, 150, 150);
-		vasak1.setFill(Color.WHITE);
-		vasak1.setStrokeWidth(15);
-		vasak1.setStroke(Color.GREEN);
-		Rectangle vasak2 = new Rectangle(15, scene.getHeight() - 155, 150, 150);
-		vasak2.setFill(Color.WHITE);
-		vasak2.setStrokeWidth(15);
-		vasak2.setStroke(Color.YELLOW);
-		Rectangle parem1 = new Rectangle(scene.getWidth() - 155, 15, 150, 150);
-		parem1.setFill(Color.WHITE);
-		parem1.setStrokeWidth(15);
-		parem1.setStroke(Color.RED);
-		Rectangle parem2 = new Rectangle(scene.getWidth() - 155,
-				scene.getHeight() - 155, 150, 150);
-		parem2.setFill(Color.WHITE);
-		parem2.setStrokeWidth(15);
-		parem2.setStroke(Color.BLUE);
-		// Lisame skoori ülesse
-		final Label skoor = new Label(String.valueOf(ulejoone));
-		skoor.setLayoutX(scene.getWidth() / 2);
-		skoor.setLayoutY(10);
-		juur.getChildren().add(skoor);
-		juur.getChildren().add(vasak1);
-		juur.getChildren().add(vasak2);
-		juur.getChildren().add(parem1);
-		juur.getChildren().add(parem2);
 
-		// Loome siin pallid ja anname neile värvid
-		for (int i = 1; i < palle + 1; i++) {
-			final Circle circle = new Circle(radius, color);
-			Physics.setRandomXY(circle);
-			if (i % (palle / 4) == 0) {
-				if (circle.getFill() == Color.YELLOW) {
-					color = Color.RED;
-				} else if (circle.getFill() == Color.RED) {
-					color = Color.GREEN;
-				} else if (circle.getFill() == Color.GREEN) {
-					color = Color.BLUE;
-				}
-			}
-			// Lisame ringidele äärised
-			circle.setStroke(Color.BLACK);
-			// Lisame võimaluse ringe lohistada
-			circle.setOnMouseDragged(new EventHandler<MouseEvent>() {
-				
-				@Override
-				public void handle(MouseEvent event) {
-					Physics.drag(circle, event.getSceneX(),
-								event.getSceneY());					
-				}
-
-			});
-			circle.setOnMouseReleased(new EventHandler<MouseEvent>() {
-
-				@Override
-				public void handle(MouseEvent event) {
-					if (!kasVäljas(circle) && !seesPallid.contains(circle)) {
-						seesPallid.add(circle);
-					} else {
-						if (seesPallid.contains(circle) && kasVäljas(circle)) {
-							seesPallid.remove(seesPallid.indexOf(circle));
-						}
-					}
-					skoor.setText("" + seesPallid.size());
-					if (seesPallid.size() == palle) {
-						mangulava.close();
-						alguslava.show();
-						tekst.setText("Tubli, " + arv.getText()
-								+ "! Suutsid "
-								+ ((System.currentTimeMillis() - startTime) / 1000)
-								+ " sekundiga \najada " + skoor.getText()
-								+ " palli kastidesse.");
-						alguslava.setScene(lõpp);
-					}
-				}
-			});
-			juur.getChildren().add(circle);
-		}
-		// Lisame võimaluse ESCAPEga sulgeda mangulava
-		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent ke) {
-				if (ke.getCode() == KeyCode.ESCAPE) {
-					mangulava.close();
-					alguslava.show();
-					tekst.setText("Kahjuks mäng sai läbi, " + arv.getText()
-							+ "! Sinu aeg oli: "
-							+ ((System.currentTimeMillis() - startTime) / 1000)
-							+ " sekundit \nja sa said " + skoor.getText()
-							+ " palli ajada kastidesse.");
-					alguslava.setScene(lõpp);
-
-				}
-			}
-		});
+		arv = new TextField("Sisesta enda nimi");
+		hb2.getChildren().add(start);
+		hb1.getChildren().add(ylesanne);
+		juur.setTop(hb1);
+		juur.setBottom(hb2);
+		juur.setCenter(arv);
 
 		alguslava.setResizable(true);
 		alguslava.setTitle("Ballpit!");
 		alguslava.setScene(algus);
 		alguslava.show();
+	}
+	
+	public static String getNimi () {
+		return arv.getText();
 	}
 
 	public static void main(String[] args) {
